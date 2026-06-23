835fb7f16840539f6f0d07c191463570fa30236a
Chanratanaya Zakaria <chanratanayazak@gmail.com>
2026-06-23 10:20:20 +0700

    Menambahkan beberapa page untuk bagian profil hanya saja belum selesai, tinggal struktur organisasi lalu guru dan staff. setelah itu bisa dilanjut dengan page lain

--- script.js
+++ script.js
@@ -73,3 +73,24 @@ document.addEventListener("DOMContentLoaded", () => {
     }
     });
 });
+
+// ini adalah bagian untuk halaman profil
+const tabs = document.querySelectorAll(".tab-btn");
+const contents = document.querySelectorAll(".tab-content");
+
+tabs.forEach(tab => {
+    tab.addEventListener("click", () => {
+    tabs.forEach(btn =>
+        btn.classList.remove("active")
+    );
+    contents.forEach(content =>
+        content.classList.remove("active")
+    );
+    tab.classList.add("active");
+    const target =
+        document.getElementById(
+        tab.dataset.tab
+        );
+    target.classList.add("active");
+});
+});
